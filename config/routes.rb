b29c87662cf9e5376aaa230a7525aa4f7c761a26
kamkara <lnclass.kamkara@gmail.com>
2022-11-12 21:58:39 +0000

    config/routes: Add User Url to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+    #Membership 
+    devise_scope :user do
+      get 'student-sign-in', to: 'devise/sessions#new'
+      get 'student-sign-up', to: 'devise/registrations#new', as: "new_user_registration"
+      delete 'deconnecter',  to: "devise/sessions#destroy", as: "destroy_user_session_path"
+    end
+
+    devise_for :users
   
-  devise_for :users
- 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
